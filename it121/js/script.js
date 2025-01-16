c261b1f7fc6b8f1e96c9a5cd99fd9aca7dc777e6
vanessa garcia <vanessa1234@hotmail.com>
2025-01-15 18:41:10 -0800

    finished hw1

---
+++ it121/js/script.js
@@ -0,0 +1,5 @@
+var myYears = "2 years";
+let firstName = "Vanessa";
+let lastName = "Garcia";
+let goal = "become a web designer";
+document.getElementById("output").innerText = "My name is" + firstName + " " + lastName + ". I have been at SCC for " + myYears + " and plan to " + goal + " after completing my courses.";
